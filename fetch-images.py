a9daefb711b2b0a49189393d6c61ac2169dbdafe
jason <jason2019au@gmail.com>
2023-04-18 14:07:43 +1000

    fixed the environment variable of aws_ecr_repo_name

--- fetch-images.py
+++ fetch-images.py
@@ -1,13 +1,12 @@
 import os
 import boto3
-# aws_ecr_repo_name = os.environ['ECR_REPO_NAME']
+aws_ecr_repo_name = os.environ['ECR_REPO_NAME']
 
 
 #get all ecr in sydney region, is default
 ecr_client=boto3.client('ecr')
 
-images=ecr_client.describe_images(repositoryName='node-app')
-#images=ecr_client.describe_images(repositoryName=aws_ecr_repo_name)
+images=ecr_client.describe_images(repositoryName=aws_ecr_repo_name)
 
 image_tags=[]
 for image in images['imageDetails']:
